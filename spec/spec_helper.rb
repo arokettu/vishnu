ce15f135a1c5de238fd9c932ffceb8496841fd1a
Anton Smirnov <sandfox@sandfox.me>
2016-04-06 05:29:05 +0300

    do not generate simplecov report if codeclimate token is set

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
-require 'simplecov'
-SimpleCov.start
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
 
 require_relative '../lib/vishnu'
