f57afb46781f068accc4dcdc3447c62d15305127
Anton Smirnov <subscr@sandfox.me>
2016-04-11 18:57:40 +0300

    really do not check coverage on JRuby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # do not check coverage on JRuby
-if RUBY_PLATFORM == 'java'
+if RUBY_PLATFORM != 'java'
   if ENV['CODECLIMATE_REPO_TOKEN']
     require 'codeclimate-test-reporter'
     CodeClimate::TestReporter.start
