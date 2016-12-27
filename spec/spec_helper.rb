a1d57cb3ed1b6f7cced8b3be80ee81817ead6f8d
Anton Smirnov <sandfox@sandfox.me>
2016-12-27 04:47:11 +0200

    change code coverage check to new scheme

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,7 @@
 # do not check coverage on JRuby
 unless RUBY_PLATFORM == 'java'
-  if ENV['CODECLIMATE_REPO_TOKEN']
-    require 'codeclimate-test-reporter'
-    CodeClimate::TestReporter.start
-  else
-    require 'simplecov'
-    SimpleCov.start
-  end
+  require 'simplecov'
+  SimpleCov.start
 end
 
 require_relative '../lib/vishnu'
